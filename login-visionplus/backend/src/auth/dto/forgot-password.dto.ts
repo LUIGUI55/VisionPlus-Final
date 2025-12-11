b8fc4aa17dfbd292155fbfa50e49d87a2e93323e
Luis Antonio Paredes Labra <luisantonio6942@gmail.com>
2025-12-11 13:42:57 -0600

    Remove login-visionplus folder from main

--- login-visionplus/backend/src/auth/dto/forgot-password.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class ForgotPasswordDto {
-    @IsEmail({}, { message: 'El email debe ser válido' })
-    @IsNotEmpty({ message: 'El email es requerido' })
-    email: string;
-}
